aebeacd999b7773ffe7386dadd4e8ab47a033d2c
bigbird <igorchornyj@gmail.com>
2024-05-07 23:35:47 +0300

    lesson1 hw

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import './App.css';
 import Characters from "./components/characters/Characters";
 import Recipes from "./components/recipes/Recipes";
+import Products from "./components/products/Products";
 
 const App = () => {
     return (
     <>
         
 {/*<Characters/>*/}
-<Recipes/>
-
+{/*<Recipes/>*/}
+<Products/>
 
     </>
     );
