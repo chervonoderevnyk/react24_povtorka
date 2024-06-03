080a3b3166a9b197c17af6cf661e250e06e9055a
bigbird <igorchornyj@gmail.com>
2024-06-03 15:29:13 +0300

    lesson5 forms+validations

--- src/components/PostFormComponent.tsx
+++ src/components/PostFormComponent.tsx
@@ -3,21 +3,34 @@ import {IPostModel} from "../models/IPostModel";
 import {FC} from "react";
 
 const PostFormComponent: FC = () => {
-const
 
 let {
       register,
       handleSubmit
     } = useForm<IPostModel>()
 
-    const AddPost = (addPost:IPostModel) => {console.log(addPost)};
+    const AddPost = (addPost:IPostModel) =>{
+        // console.log(addPost)
+
+        fetch('https://dummyjson.com/posts/add', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                title: addPost.title,
+                userId: addPost.userId,
+            })
+        })
+            .then(res => res.json())
+        .then(data=> {
+            console.log("Responce:", data)});
+};
 
     return (
         <div>
             <form onSubmit={handleSubmit(AddPost)}>
-                <input type={'text'} {...register("title")}/>
+                <input type={'text'} {...register("title")} placeholder={"title"}/>
                 <br/>
-                <input type={"number"} {...register("userId")}/>
+                <input type={"number"} {...register("userId")} placeholder={"user Id"}/>
                 <br/>
                 <button>Add post</button>
 
