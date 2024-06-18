4db260c5d03a5432899ef682f27d0facaf24cb47
bigbird <igorchornyj@gmail.com>
2024-06-18 13:33:03 +0300

    lesson6 routes double3

--- src/routers/router.tsx
+++ src/routers/router.tsx
@@ -1,7 +1,21 @@
 import {createBrowserRouter} from "react-router-dom";
 import {MainLayout} from "../layouts/MainLayout";
+import {HomePage} from "../pages/HomePage";
+import {UserPage} from "../pages/UserPage";
+import {PostPage} from "../pages/PostPage";
+import {CommentPage} from "../pages/CommentPage";
 
 export const router = createBrowserRouter([
-    {path: '/', element: <MainLayout/>}
+    {
+        path: '/',
+        element: <MainLayout/>,
+        errorElement: <h2>what are you douing maaaan????</h2>,
+        children: [
+            {index:true, element: <HomePage/>},
+            {path: '/users', element:<UserPage/>},
+            {path: 'posts', element:<PostPage/>},
+            {path: 'comments', element:<CommentPage/>},
+        ]
+    }
 ])
 
