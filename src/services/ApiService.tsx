c7d0620ba47c1d58ec1ce98eb3f038e4bd0121f3
bigbird <igorchornyj@gmail.com>
2024-06-21 11:39:15 +0300

    lesson6 routes double3

--- src/services/ApiService.tsx
+++ src/services/ApiService.tsx
@@ -1,6 +1,15 @@
-import axios from "axios";
+import axios, {AxiosResponse} from "axios";
+import {IUserModel} from "../models/IUserModel";
 
-axios.create(
-    baseURL: 'https://jsonplaceholder.typicode.com/',
+axios.create({
+        baseURL: 'https://jsonplaceholder.typicode.com/',
+        headers:
+    });
 
-);
+const userApiService = {
+
+    getAllUsers: (): Promise<AxiosResponse<IUserModel[]>> => {
+        return axios.get("/users");
+    },
+
+    }
