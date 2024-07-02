1209e00c37c32e1164d1a82a62a254ce717f3cc0
bigbird <igorchornyj@gmail.com>
2024-07-02 19:15:39 +0300

    lesson9 auth2

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -4,35 +4,31 @@ import {authService} from "../services/ApiService";
 import {useState} from "react";
 
 const FormComponent = () => {
-
     const {
         handleSubmit,
         register
-    } = useForm<TokenObtainPair>({defaultValues: {username: 'userChI', password: 'Pa$$word22'}});
+    } = useForm<TokenObtainPair>({defaultValues: {username: 'userChI', password: "Pa$$word22"}});
 
-    const [isAuthState, setIsAuthState] = useState<boolean>(false);
+    const [isAuthState , setIsAuthState ] = useState<boolean>(false)
 
-    const authenticate = async (formData: TokenObtainPair): Promise<number> => {
-        const isAuth = await authService.authentication(formData);
-        setIsAuthState(isAuth);
-
-        return 100500;
+    const authenticate = async (formData: TokenObtainPair) => {
+        const isAuth =await authService.authentication(formData)
+        setIsAuthState(isAuth)
     };
+
     return (
         <div>
-            <h3>login form</h3>
-            <div>
-                {
-                    isAuthState ? <span>ok</span> : <span>not ok</span>
-                }
+            <h3>
+                <div>
+                    {isAuthState ? <span>ok</span> : <span>not ok</span>}
+                </div>
+            </h3>
 
-            </div>
             <form onSubmit={handleSubmit(authenticate)}>
                 <input type="text" {...register('username')}/>
-                <input type="text" {...register('password')}/>
+                <input type="text"{...register('password')}/>
                 <button>auth me</button>
             </form>
-
         </div>
     );
 };
